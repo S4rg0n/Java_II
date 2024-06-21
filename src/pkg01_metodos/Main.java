c0fdee2a8840b24fbceec72a9e8def451e969632
Moisés Arce Salmón <arcmonmassesa@gmail.com>
2024-06-21 12:59:30 +0200

    pkg01 Ejemplarización de sobrecarga de método "suma"
    Se crean 3 métodos "suma" con distintos parámetros de entrada (double, int y String respectivamente).

--- src/pkg01_metodos/Main.java
+++ src/pkg01_metodos/Main.java
@@ -5,7 +5,13 @@ public class Main {
 
         Utilidades accion = new Utilidades();
         accion.saludar();
+
+        // Ante sobrecarga de método, Java determina que tipos de parámetros se introducen y escoge el adecuado.
         double resultado = accion.suma(1,5);
         System.out.println("La suma de 1 y 5 es  1+5 = "+resultado);
+        resultado = accion.suma(2.5,5);
+        System.out.println("La suma de 2.5 y 5 es  2.5+5 = "+resultado);
+        resultado = accion.suma("1","5");
+        System.out.println("La suma de 1 y 5 pasados como string es  1+5 = "+resultado);
     }
 }
--- src/pkg01_metodos/Utilidades.java
+++ src/pkg01_metodos/Utilidades.java
@@ -8,11 +8,22 @@ public class Utilidades {
     }
 
     // Método: presenta parámetros, que recibirán unos argumentos (valores) para funcionar
-    public double suma(int num1, int num2){
+    public int suma(double num1, double num2){
         double resultado = 0;
         resultado = num1 + num2;
-        return resultado; // Siempre tiene un return. Éste puede dvolver un valor o no, pero ha de existir.
+        return (int)resultado; // Siempre tiene un return. Éste puede dvolver un valor o no, pero ha de existir.
         // Lo que siguiera del return NO se ejecuta.
+    }
+    // La sobrecarga de métodos nos permite asignar un mismo nombre de método para distintas parametrizaciones:
+    public int suma(int num1, int num2){
+        int resultado = 0;
+        resultado = num1+num2;
+        return resultado;
+    }
+    public int suma(String num1, String num2){
+        int resultado = 0;
+        resultado =  Integer.parseInt(num1) + Integer.parseInt(num2);
+        return resultado;
     }
 
 }
