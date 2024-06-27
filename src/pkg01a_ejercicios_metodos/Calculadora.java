a918c64e9ad81492b5026af4f3b8c03d826dba39
Moisés Arce Salmón <arcmonmassesa@gmail.com>
2024-06-27 12:54:04 +0200

    Ejercicio clase Calculadora en  pkg01a
     Calculadora diseñada como clase para simplificar el código en Main. Para hacer uso del encapsulamiento, sólo el método iniciar será accesible desde Main.

---
+++ src/pkg01a_ejercicios_metodos/Calculadora.java
@@ -0,0 +1,71 @@
+package pkg01a_ejercicios_metodos;
+
+import javax.swing.*;
+
+public class Calculadora {
+    int i = 1;
+    private double pedirOperando(){
+        i++;
+        return Double.parseDouble(JOptionPane.showInputDialog("Introduzca operando "+(i-1)+":"));
+    }
+
+    private String pedirOperacion(){
+        return JOptionPane.showInputDialog(
+                "Qué operación desea realizar??\n" +
+                "+: suma\n" +
+                "-: resta\n" +
+                "*: multiplica\n" +
+                "/: divide\n" +
+                "%: módulo (resto)");
+    }
+
+    private double suma(double op1, double op2){
+        return op1+op2;
+    }
+
+    private double resta(double op1, double op2){
+        return op1-op2;
+    }
+
+    private double multiplica(double op1, double op2){
+        return op1*op2;
+    }
+
+    private double divide(double op1, double op2){
+        return op1/op2;
+    }
+
+    private int resto(int op1, int op2) {
+        return op1%op2;
+    }
+
+    private void resolucion(String signo, double op1, double op2){
+        switch (signo){
+            case "+":
+                JOptionPane.showMessageDialog(null, op1+" + "+op2+" = "+ suma(op1,op2));
+                break;
+            case "-":
+                JOptionPane.showMessageDialog(null, op1+" - "+op2+" = "+ resta(op1,op2));
+                break;
+            case "*":
+                JOptionPane.showMessageDialog(null, op1+" * "+op2+" = "+ multiplica(op1,op2));
+                break;
+            case "/":
+                JOptionPane.showMessageDialog(null, op1+" / "+op2+" = "+ divide(op1,op2));
+                break;
+            case "%":
+                JOptionPane.showMessageDialog(null, "Resto de dividir "+op1+" / "+op2+" es "+ resto((int)op1,(int)op2));
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Datos erróneos, finalizando programa.");
+        }
+    }
+
+    public void iniciar(){
+        double op1 = pedirOperando();
+        String signo = pedirOperacion();
+        double op2 = pedirOperando();
+        resolucion(signo,op1,op2);
+    }
+
+}
--- src/pkg01a_ejercicios_metodos/Main.java
+++ src/pkg01a_ejercicios_metodos/Main.java
@@ -3,20 +3,21 @@ package pkg01a_ejercicios_metodos;
 public class Main {
     public static void main(String[] args) {
 
-//  1) Crear un programa que haga 10 siguiente:
-//     Crear una clase llamada "Automóvil", esta clase contará con los siguientes características:
-//      Atributos:
-//        marca (String)
-//        color (String)
-//        potencia (int)
-//        precio (double)
-//      Métodos:
-//        Getters y Setters para cada atributo.
-//        Un método que permita visualizar todas las características del automóvil. (ya sea por
-//        consola o con JOptionPane)
-
-//        Finalmente crear una clase que contenga el método "public static void main" y crea un par de
-//        objetos de la clase Automóvil. Asígnale valores a los objetos y prueba el programa:
+/*
+  1) Crear un programa que haga 10 siguiente:
+     Crear una clase llamada "Automóvil", esta clase contará con los siguientes características:
+      Atributos:
+        marca (String)
+        color (String)
+        potencia (int)
+        precio (double)
+      Métodos:
+        Getters y Setters para cada atributo.
+        Un método que permita visualizar todas las características del automóvil. (ya sea por
+        consola o con JOptionPane)
+        Finalmente crear una clase que contenga el método "public static void main" y crea un par de
+        objetos de la clase Automóvil. Asígnale valores a los objetos y prueba el programa:
+*/
 
         Automovil coche1 = new Automovil();
         coche1.setMarca("Seat");
@@ -33,16 +34,31 @@ public class Main {
         System.out.println(coche2.toString());
 
 
-//  2) Lee un número por teclado que pida el precio de un producto (puede tener
-//     decimales) y calcule el precio final con IVA. EI IVA será del 21%.
-//     Utilizar métodos. Por ejemplo, un método encargado de pedir por teclado, otro
-//     método que se encargue del calculo y otro método encarga#o de mostrar el resultado.
-
+/*
+  2) Lee un número por teclado que pida el precio de un producto (puede tener
+     decimales) y calcule el precio final con IVA. EI IVA será del 21%.
+     Utilizar métodos. Por ejemplo, un método encargado de pedir por teclado, otro
+     método que se encargue del calculo y otro método encargado de mostrar el resultado.
+*/
         CalcularPrecio valor = new CalcularPrecio();
         valor.mostrarPrecioFinal();
 
 
 
+/*
+  3) Crea una aplicaciön tipo calculadora, nos pedirå 2 operandos (int) y un signo
+     aritmético (String), segün este éltimo se realizara la operación correspondiente. Al
+     final mostrara el resultado en un cuadro de diálogo (JOptionPane).
+     Los signos aritméticos disponibles son:
+        +: suma los dos operandos.
+        -: resta los operandos.
+        *:multiplica los operandos.
+        /: divide los operandos, este debe dar un resultado con decimales (double)
+        %: mödulo, resto de la divisiön entre operandol y operando2.
+    Utilizar métodos para realizar el ejercicio. Por ejemplo, un método para cada acción.
+*/
+        Calculadora calculo = new Calculadora();
+        calculo.iniciar();
 
     }
 }
