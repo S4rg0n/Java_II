308f194828ce52b89e73bb44361d91afb44e6910
Moisés Arce Salmón <arcmonmassesa@gmail.com>
2024-07-01 19:21:01 +0200

    Inicio ordenacion por seleccion

--- src/pkg02_matrices/pkg02c_algoritmos_ordenacion/OrdenacionSeleccion.java
+++ src/pkg02_matrices/pkg02c_algoritmos_ordenacion/OrdenacionSeleccion.java
@@ -1,4 +1,27 @@
 package pkg02_matrices.pkg02c_algoritmos_ordenacion;
 
 public class OrdenacionSeleccion {
+    public static void main(String[] args) {
+
+        int[] matriz = {26, 50, 7, 9, 15, 27, 8};
+
+        System.out.println("\nMatriz sin ordenar: ");
+        for (int num : matriz){
+            System.out.print(num==matriz[matriz.length-1] ? num+"\n\n" : num+", ");
+        }
+        seleccion(matriz);
+        System.out.println("\nMatriz ordenada por método burbuja: ");
+        for (int num : matriz){
+            System.out.print(num==matriz[matriz.length-1] ? num+"\n" : num+", ");
+        }
+    }
+
+    private static void seleccion(int[] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            int reemplazo = matriz[i];
+            for (int j = i+1 ; j < matriz.length; j++) {
+                
+            }
+        }
+    }
 }
